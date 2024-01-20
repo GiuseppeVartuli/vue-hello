4b60548bebac36847210326bb87de1ac2bf9b191
GiuseppeVartuli <giuseppevartuli89@gmail.com>
2024-01-20 18:47:06 +0100

    printed in dom of the player's name and photo

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      player: "John Terry",
+      img: "https://static.fctables.com/upload/w6/w6o5z58bef68f1cf81/john-terry.png",
+    };
+  },
+};
+</script>
+
+<template>
+  <h1>{{ player }}</h1>
+  <div>
+    <img :src="img" alt="" />
+  </div>
+</template>
+
+<style></style>
